eb2bbf4858918fcd398fa537011564dca2684f8a
Daniel Talik <danieltalik@gmail.com>
2018-08-13 11:13:48 -0400

    Added Cat Model

---
+++ c-final-capstone-v2/Models/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace c_final_capstone_v2.Models
+{
+    public class Cat
+    {
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public List<string> Color { get; set; }
+        public string HairLenth { get; set; }
+        public string PriorExperience { get; set; }
+        public string PictureId { get; set; }
+        public List<string> Skills { get; set; }
+    }
+}
