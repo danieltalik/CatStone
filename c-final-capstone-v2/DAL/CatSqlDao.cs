c7adea5f750a38ff17106803d23eb3f6f43c3bc7
Daniel Talik <danieltalik@gmail.com>
2018-08-14 14:49:23 -0400

    Preparing for pull at EOD Tuesday

--- c-final-capstone-v2/DAL/CatSqlDao.cs
+++ c-final-capstone-v2/DAL/CatSqlDao.cs
@@ -11,20 +11,8 @@ namespace c_final_capstone_v2.DAL
     public class CatSqlDao : ICatSqlDao
     {
         private const string SQL_All_Cats = "SELECT * FROM Cats";
-<<<<<<< HEAD
-        private const string SQL_AddCats = "INSERT INTO Cats (name, color, hair_length, age, prior_exp, photo, description ) VALUES (@name, @color, @hair_length, @age, @prior_exp, @description )";
-        private const string SQL_ViewCat = "SELECT * FROM cats WHERE cat.Id = @Id";
-        private const string SQL_RemoveCat = "";//UNDONE
-
-        private ISkillDao dao;
-        string connectionString = ConfigurationManager.ConnectionStrings["CatStoneConnection"].ConnectionString;
-
-        public CatSqlDao()
-        {
-            this.dao = new SkillDao(connectionString);
-=======
         private const string SQL_AddCats = "INSERT INTO Cats (name, color, hair_length, age, prior_exp, photo, description ) VALUES (@name, @color, @hair_length, @age, @prior_exp, @photo, @description )";
-        private const string SQL_ViewCat = "SELECT * FROM cats WHERE cat.name = @name";
+        private const string SQL_ViewCat = "SELECT * FROM cats WHERE Id = @ID";
         private const string SQL_RemoveCat = "";//UNDONE
 
         private ISkillDao dao;
@@ -34,11 +22,9 @@ namespace c_final_capstone_v2.DAL
         {
             this.connectionString = connectionString;
             dao = new SkillDao(connectionString);
->>>>>>> f6c5addb2a466d98b69c35404542b3c0d4b3dd65
+
         }
-
         
-
         public List<Cat> GetAllCats()
         {
             List<Cat> cats = new List<Cat>();
@@ -76,7 +62,7 @@ namespace c_final_capstone_v2.DAL
                     conn.Open();
                     SqlCommand command = new SqlCommand(SQL_ViewCat);
                     command.Connection = conn;
-
+                    command.Parameters.AddWithValue("@ID", id);
                     SqlDataReader reader = command.ExecuteReader();
 
                     while (reader.Read())
