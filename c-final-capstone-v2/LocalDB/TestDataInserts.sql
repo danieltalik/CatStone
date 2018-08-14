8baf276ab5be1ad0da94e601df56aaa471a7ff3b
austin.WXJ <austin.WXJ@gmail.com>
2018-08-14 14:03:32 -0400

    TestDataInserts.sql added drop and create table DDL

--- c-final-capstone-v2/LocalDB/TestDataInserts.sql
+++ c-final-capstone-v2/LocalDB/TestDataInserts.sql
@@ -1,4 +1,10 @@
 ﻿----DELETE * FROM skills
+DROP TABLE skills
+CREATE TABLE [dbo].[Skills] (
+    [id]    INT          IDENTITY (1, 1) NOT NULL,
+    [skill] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Table] PRIMARY KEY CLUSTERED ([id] ASC)
+);
 INSERT INTO SKILLS (skill) VALUES('Drooling')
 INSERT INTO SKILLS (skill) VALUES('Driving')
 INSERT INTO SKILLS (skill) VALUES('Efficiency expert')
@@ -16,6 +22,19 @@ INSERT INTO SKILLS (skill) VALUES('Life Coach')
 
 
 ----DELETE * FROM Cats
+DROP TABLE Cats
+CREATE TABLE [dbo].[Cats] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [name]        VARCHAR (50)  NULL,
+    [color]       VARCHAR (50)  NULL,
+    [hair_length] VARCHAR (50)  NULL,
+    [age]         INT           NULL,
+    [prior_exp]   VARCHAR (250) NULL,
+    [photo]       VARCHAR (50)  NULL,
+    [is_featured] BIT           DEFAULT ((0)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
 INSERT INTO Cats (name, color, hair_length, age, prior_exp, photo, is_featured ) VALUES ('Percy','orange $ white', 'Long', 12, 'Purficient at wakeup calls', 'Percy.jpg', 1 )
 INSERT INTO Cats (name, color, hair_length, age, prior_exp, photo, is_featured ) VALUES ('Fern', 'tabby', 'long', 8, 'Will do anything for bread.', 'fern.jpg', 0 )
 INSERT INTO Cats (name, color, hair_length, age, prior_exp, photo, is_featured ) VALUES ('Luna', 'Black & Orange', 'short', 3, 'Flexible for the right opportunity', 'luna.jpg', 0 )
@@ -26,6 +45,12 @@ INSERT INTO Cats (name, color, hair_length, age, prior_exp, photo, is_featured )
 --INSERT INTO Cats (name, color, hair_length, age, prior_exp, photo, is_featured ) VALUES ('', '',, '', '', 0)
 
 ----DELETE * FROM cat_skill
+DROP TABLE cat_skills
+CREATE TABLE [dbo].[cat_skill] (
+    [cat_id]   INT NOT NULL,
+    [skill_id] INT NOT NULL,
+    CONSTRAINT [PK_Cats_Skills_cat_id_skill_id] PRIMARY KEY CLUSTERED ([cat_id] ASC, [skill_id] ASC)
+);
 INSERT INTO cat_skill (cat_id, skill_id) VALUES(1, 1)
 INSERT INTO cat_skill (cat_id, skill_id) VALUES(1, 7)
 INSERT INTO cat_skill (cat_id, skill_id) VALUES(2, 6)
@@ -36,6 +61,14 @@ INSERT INTO cat_skill (cat_id, skill_id) VALUES(3, 9)
 --INSERT INTO cat_skill (cat_id, skill_id) VALUES(, )
 
 ----DELETE * FROM 
+DROP TABLE Users
+CREATE TABLE [dbo].[Users] (
+    [Id]       INT          IDENTITY (1, 1) NOT NULL,
+    [name]     NCHAR (10)   NULL,
+    [password] VARCHAR (50) NULL,
+    [is_admin] BIT          DEFAULT ((0)) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
 INSERT INTO Users (name, password, is_admin) VALUES ('Dinah',			'password', 1)
 INSERT INTO Users (name, password, is_admin) VALUES ('Peter',			'password', 1)
 INSERT INTO Users (name, password, is_admin) VALUES ('Dan',				'password', 1)
