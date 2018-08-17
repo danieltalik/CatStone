0dd80940ab0fd4b5bf979422709b6fe0d2f9a422
Daniel Talik <danieltalik@gmail.com>
2018-08-17 10:24:44 -0400

    Work on user controller

--- c-final-capstone-v2/Controllers/UserController.cs
+++ c-final-capstone-v2/Controllers/UserController.cs
@@ -27,24 +27,32 @@ namespace c_final_capstone_v2.Controllers
         [HttpPost]
         public ActionResult UserHome(LoginModel login)
         {
+            //We need to have parameters where session is NOT NULL and checks and redirects to the right view
 
-            //Will be centered around IUserDao and is a hard coded workaround
+            //Needs to check if null and build a staff member if session is null and they logged in
             Staff staff = userDao.Login(login.Username, login.Password);
 
             Session["User"] = staff.IsAdmin;
-
+            //If session is null and user login is valid and not an admin
             if (!(bool)Session["User"])
             {
                 return View("StaffView");
             }
+            //Returns admin view if session is null and builds it
             else
             {
                 return View("AdminView");
             }
+            //We also need to check if Model state is vaild and if it is not then redirect to login with error message
         }
         public ActionResult NewStaffView()
         {
-            if ((bool)Session["User"])
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login");
+
+            }
+            else if ((bool)Session["User"])
             {
                 return View();
             }
@@ -54,6 +62,7 @@ namespace c_final_capstone_v2.Controllers
         [HttpPost]
         public ActionResult SubmitStaff(Staff newStaff)//TODO tmove to admin controller
         {
+            //Fix Issue where refresh adds new staff entry over and over
             AdminDao admin = new AdminDao(connectionString);
             admin.AddStaff(newStaff);
             return View("AdminView");
